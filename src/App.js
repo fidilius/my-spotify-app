9d3683ecea3ada340d4d2932108595013430f0df
fidilius <fidilius.siregar@gmail.com>
2022-03-22 23:51:48 +0700

    add data.js update App.js App.css index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data/data.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='song'>
+        <img src={data.album.images[1].url} alt="" />
+        <h2>{data.album.name}</h2>
+        <h3>{data.artists[0].name}</h3>
+        <button>Select</button>
+      </div>
     </div>
   );
 }
