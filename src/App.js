95907afb9f6ce2eb9f8221fdafa0bce559cc7164
fidilius <fidilius.siregar@gmail.com>
2022-03-26 23:09:33 +0700

    update App.js, App.css, component track, data.js & return index.css as before

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Track from './components/track';
+import data from './data/data.js';
 
 function App() {
   return (
     <div className="App">
-      <Track />
+      <div className="tracks">
+        {data.map((track) => {
+          return <Track key={track.album.id} srcAlbum={track.album.images[1].url} textTitle={track.album.name} textArtist={track.artists[0].name}/>
+        })}
+      </div>
     </div>
   );
 }
--- src/components/track/index.js
+++ src/components/track/index.js
@@ -1,15 +1,14 @@
 import './index.css';
-import data from '../../data/data.js';
 import {H2, H3} from '../header';
 import Image from '../image';
 import Button from '../button';
 
-const track = () => {
+const track = ({srcAlbum, textTitle, textArtist}) => {
     return (
         <div className='track'>
-            <Image src={data.album.images[1].url} alt="" />
-            <H2 text={data.album.name} />
-            <H3 text={data.artists[0].name} />
+            <Image src={srcAlbum} alt={textTitle} />
+            <H2 text={textTitle} />
+            <H3 text={textArtist} />
             <Button text={'Select'} />
         </div>
     );
