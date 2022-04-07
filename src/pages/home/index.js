847cc48ecf632b9dc1e448e196ca702de6cf4074
fidilius <fidilius.siregar@gmail.com>
2022-04-07 22:08:43 +0700

    create react router - update page home, move create playlist to url /create-playlist

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,13 +3,30 @@ import data from '../../data/data.js';
 import Track from '../../components/track'
 import Login from '../../components/Login';
 import Search from '../../components/Search';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 const Home = () => {
+    const isLogin = useSelector(state => state.token.access_token) !== '';
+    console.log(isLogin);
+
+
     return(
         <div>
             <div className="search">
-                <Login />
-                <Search />
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                            <Login />
+                            {isLogin && <Redirect exact from={window.location.href} to="/create-playlist"/>}
+                        </Route>
+
+                        <Route exact path="/create-playlist">
+                            {isLogin ? <Search /> : <Redirect exact from="/create-playlist" to="/" />}
+                        </Route>
+                    </Switch>
+                </Router>
             </div>
             <div className="tracks">
                 {data.map((track, index) => {
